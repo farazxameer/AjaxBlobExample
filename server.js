84f3452f8b7edec130d88f11d6b9fa2b09be1aee
Richard Astbury <Richard.Astbury@gmail.com>
2012-08-14 09:09:32 +0100

    added code to create table

--- server.js
+++ server.js
@@ -4,24 +4,29 @@ app.enable("jsonp callback");
 
 var azureAccount = "two10ra";
 var azureKey = "dmIMUY1mg/qPeOgGmCkO333L26cNcnUA1uMcSSOFMB3cB8LkdDkh02RaYTPLBL8qMqnqazqd6uMxI2bJJEnj0g==";
-
+var containerName = "container"
+var sessionLifetime = 1; // hours
 
 var blobService = azure.createBlobService(azureAccount, azureKey);
+blobService.createContainerIfNotExists(containerName, function(error){
+	if (error){
+		console.error(error);
+	}
+});
 
 app.get('/login/:username', function(req, res){
-	var url = blobService.generateSharedAccessSignature("foo", req.params.username, {
+	var url = blobService.generateSharedAccessSignature(containerName, req.params.username, {
 	AccessPolicy : {
 		Permissions : "rwdl",
-		Expiry : getDate()
+		Expiry : getDate();
 	}});
 	res.json({url: url.url()});
-
 });
 
 app.listen(process.env.port || 210);	
 
 function getDate(){
 	var date = new Date();
-	date.setHours(date.getHours()+1);
+	date.setHours((date).getHours() + sessionLifetime);
 	return date;
 }
